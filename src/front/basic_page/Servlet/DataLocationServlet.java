0c0ce7924daec076001c319ad12b52e9fd59334d
wwx_s <46552185+rainnns@users.noreply.github.com>
2020-06-17 08:48:56 +0800

    数据空间位置展示

--- src/front/basic_page/Servlet/DataLocationServlet.java
+++ src/front/basic_page/Servlet/DataLocationServlet.java
@@ -1,11 +1,17 @@
 package front.basic_page.Servlet;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import front.basic_page.Dao.QueryData;
+import front.basic_page.Domain.BasicInfoPos;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 
 /**
  * 描述:
@@ -19,8 +25,30 @@ import java.io.IOException;
 public class DataLocationServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.getParameter("type");
-        req.getParameter("tagName");
+        Map<String,String[]> map = req.getParameterMap();
+        String type = map.getOrDefault("type", new String[]{"2"})[0];
+        int id = Integer.parseInt(map.getOrDefault("tagId", new String[]{"9"})[0]);
+
+//        String type = req.getParameter("type");
+//        String idString = req.getParameter("tagId");
+//        int id = Integer.parseInt(idString);
+
+        QueryData queryData = new QueryData();
+        List<BasicInfoPos> basicInfoPos = null;
+        if (type.equals("2")){
+            basicInfoPos = queryData.QueryBasicInfoByTagLevel2(id);
+        }
+        else if (type.equals("1")){
+            basicInfoPos = queryData.QueryBasicInfoByTagLevel1(id);
+        }
+
+        ObjectMapper mapper = new ObjectMapper();
+        String result = mapper.writeValueAsString(basicInfoPos);
+
+        resp.setContentType("text/html;charset=utf-8");
+        resp.setHeader("Access-Control-Allow-Origin", "*");//解决跨域问题，开发完毕时应该关闭
+        resp.getWriter().append(result);
+
     }
 
     @Override
@@ -28,7 +56,4 @@ public class DataLocationServlet extends HttpServlet {
         this.doPost(req,resp);
     }
 
-    public void FirstLevel(String tagName){
-
-    }
 }
