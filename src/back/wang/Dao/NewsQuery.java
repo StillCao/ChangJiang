ef858d6101fa9f603cce8ac52e4bee0f8b68cba4
wwx_s <46552185+rainnns@users.noreply.github.com>
2020-06-30 14:55:05 +0800

    新闻接口

--- src/back/wang/Dao/NewsQuery.java
+++ src/back/wang/Dao/NewsQuery.java
@@ -38,46 +38,77 @@ public class NewsQuery {
     }
 
     /**
-     *
      * @return 查询news表条数
      */
-    public int queryNewsCount(){
+    public int queryNewsCount() {
         String sql = "select count(*) from news";
-        return template.queryForObject(sql,Integer.class);
+        return template.queryForObject(sql, Integer.class);
     }
 
     /**
-     *
      * @return 查询news表条数
      */
-    public int queryNewsByKeyCount(String key, String value){
+    public int queryNewsByKeyCount(String key, String value) {
         value = "%" + value + "%";
         String sql = "select count(*) from news where " + key + " like ?";
-        return template.queryForObject(sql,Integer.class,value);
+        return template.queryForObject(sql, Integer.class, value);
     }
 
     /**
-     *
      * @param startPos 起始位置
-     * @param count 查询条数
+     * @param count    查询条数
      * @return news集合
      */
-    public List<News> queryAllNewsByPage(int startPos, int count){
+    public List<News> queryAllNewsByPage(int startPos, int count) {
         String sql = "select * from news limit ?,?";
         return template.query(sql, new BeanPropertyRowMapper<>(News.class), startPos, count);
     }
 
     /**
      * 插入一条news
+     *
      * @param news
      * @return
      */
-    public boolean insertNews(News news){
+    public boolean insertNews(News news) {
         String sql = "insert into news (id,title,DATE,source,TYPE,news_cont,localaddr) Values(null,:title,:DATE,:source,:TYPE,:news_cont,:localaddr)";
         KeyHolder keyHolder = new GeneratedKeyHolder();
 
         npjTemplate.update(sql, new BeanPropertySqlParameterSource(news), keyHolder);
         return keyHolder.getKey().intValue() > 0;
+    }
+
+    /**
+     * 根据id 删除news
+     *
+     * @param id news id
+     * @return 是否删除成功
+     */
+    public boolean deleteNews(int id) {
+        String sql = "delete from news where id = ?";
+        return template.update(sql, id) > 0;
+    }
+
+    /**
+     * 根据id 查询news 的图片存储位置
+     *
+     * @param id news id
+     * @return 图片在服务器上的绝对位置
+     */
+    public String queryNewsAddr(int id) {
+        String sql = "select localaddr from news where id = ?";
+        return template.queryForObject(sql, String.class, id);
+    }
+
+    /**
+     * 根据news 的id，更新news
+     *
+     * @param news id大于0的News对象
+     * @return 是否修改成功
+     */
+    public boolean updateNewsById(News news) {
+        String sql = "UPDATE news SET title = ?, DATE = ?, source = ?, TYPE = ?, news_cont = ? WHERE id = ?";
+        return template.update(sql, news.getTitle(), news.getDATE(), news.getSource(), news.getTYPE(), news.getNews_cont(), news.getId()) > 0;
     }
 
 
--- src/back/wang/Test/NewsTest.java
+++ src/back/wang/Test/NewsTest.java
@@ -4,6 +4,7 @@ import back.wang.Dao.NewsQuery;
 import back.wang.Domain.News;
 import back.wang.Service.NewsService;
 
+import java.io.File;
 import java.util.Date;
 import java.util.List;
 
@@ -18,7 +19,7 @@ import java.util.List;
 public class NewsTest {
     @org.junit.Test
     public void queryAllAdmins() {
-        List<News> news = new NewsQuery().queryNewsByKeyLikeByPage("title","国家",0,10);
+        List<News> news = new NewsQuery().queryNewsByKeyLikeByPage("title", "国家", 0, 10);
         System.out.println(news.size());
     }
 
@@ -29,12 +30,12 @@ public class NewsTest {
 
     @org.junit.Test
     public void queryAllNewsByPage() {
-        System.out.println(new NewsQuery().queryAllNewsByPage(0,10));
+        System.out.println(new NewsQuery().queryAllNewsByPage(0, 10));
     }
 
     @org.junit.Test
     public void queryNewsByKeyCount() {
-        System.out.println(new NewsQuery().queryNewsByKeyCount("title","地球"));
+        System.out.println(new NewsQuery().queryNewsByKeyCount("title", "地球"));
     }
 
     @org.junit.Test
@@ -54,13 +55,41 @@ public class NewsTest {
 
     @org.junit.Test
     public void allNews() {
-        System.out.println(new NewsService().allNews(1,10));
+        System.out.println(new NewsService().allNews(1, 10));
     }
 
     @org.junit.Test
     public void newsByKey() {
-        System.out.println(new NewsService().newsByKey("title","地球",1,10));
+        System.out.println(new NewsService().newsByKey("title", "地球", 1, 10));
     }
 
+    @org.junit.Test
+    public void deleteNews() {
+        System.out.println(new NewsQuery().deleteNews(19));
+    }
+
+    @org.junit.Test
+    public void queryNewsAddr() {
+        System.out.println(new NewsQuery().queryNewsAddr(18));
+    }
+
+    @org.junit.Test
+    public void updateNewsById() {
+        News news = new News();
+        news.setId(18);
+        news.setTYPE(1);
+        news.setLocaladdr("asdqwd");
+        news.setNews_cont("dwqdwqdwqdwqdqwdqwd");
+        news.setTitle("qwd");
+        news.setSource("asdqd");
+        news.setDATE(new Date());
+        System.out.println(new NewsQuery().updateNewsById(news));
+    }
+
+    @org.junit.Test
+    public void changFileFolderName() {
+        File filefolder = new File("D://长江中游地学数据集");
+        filefolder.renameTo(new File(filefolder.getParent() + "ahah"));
+    }
 
 }
