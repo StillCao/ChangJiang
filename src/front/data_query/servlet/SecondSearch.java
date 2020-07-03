e44765ed883890246e562726ff8b6143d23c5eb7
admin <727106120@qq.com>
2020-07-03 20:11:48 +0800

    数据查询

--- src/front/data_query/servlet/SecondSearch.java
+++ src/front/data_query/servlet/SecondSearch.java
@@ -24,6 +24,14 @@ public class SecondSearch extends HttpServlet {
         resp.setContentType("text/html;charset=utf-8");
 
         //2.获取请求参数
+        //2.1 获取文件列表分页查询的请求参数
+        String updated = req.getParameter("updated");
+        boolean update = Boolean.parseBoolean(updated);
+        String pageSize = req.getParameter("PageSize");
+        String currentPage = req.getParameter("currentPage");
+        int page = Integer.parseInt(pageSize);
+        int curpage = Integer.parseInt(currentPage);
+
         String searchWord = req.getParameter("searchWord");
         String startTime = req.getParameter("startTime");
         String endTime = req.getParameter("endTime");
@@ -32,30 +40,66 @@ public class SecondSearch extends HttpServlet {
         //3.1 先取出上一步得到的未分页的文件列表中的数据list集合
         List<Basic_info> infoList = new ArrayList<>();
         HttpSession ses = req.getSession();
-        List<Map> doclist_nopage = (List<Map>) ses.getAttribute("doclist_nopage");
-        for (Map map : doclist_nopage) {
-            infoList = (List<Basic_info>) map.get("features");
-        }
-
         List<Map> mapList = new ArrayList<>();
-        if(startTime != null && endTime != null){
-            int start = Integer.parseInt(startTime);
-            int end = Integer.parseInt(endTime);
-            mapList = new Query().search_sec(searchWord, searchWord, start, end, infoList);
-        }else {
-            if(startTime == null && endTime == null){
-                int start = 0;
-                int end = 0;
+
+        List<Map> doclist_nopage = (List<Map>) ses.getAttribute("doclist_nopage");
+        if (doclist_nopage != null){
+            //与之前查询结果相关
+            ObjectMapper mapper1= new ObjectMapper();
+            System.out.println("SecondSearch?doclist_nopage="+mapper1.writeValueAsString(doclist_nopage));
+
+            for (Map map : doclist_nopage) {
+                infoList = (List<Basic_info>) map.get("features");
+            }
+
+            if(startTime != null && endTime != null){
+                int start = Integer.parseInt(startTime);
+                int end = Integer.parseInt(endTime);
                 mapList = new Query().search_sec(searchWord, searchWord, start, end, infoList);
             }else {
-                if (startTime == null && endTime != null){
+                if(startTime == null && endTime == null){
                     int start = 0;
-                    int end = Integer.parseInt(endTime);
-                    mapList = new Query().search_sec(searchWord, searchWord, start, end, infoList);
-                }else {
-                    int start = Integer.parseInt(startTime);
                     int end = 0;
                     mapList = new Query().search_sec(searchWord, searchWord, start, end, infoList);
+                }else {
+                    if (startTime == null && endTime != null){
+                        int start = 0;
+                        int end = Integer.parseInt(endTime);
+                        mapList = new Query().search_sec(searchWord, searchWord, start, end, infoList);
+                    }else {
+                        int start = Integer.parseInt(startTime);
+                        int end = 0;
+                        mapList = new Query().search_sec(searchWord, searchWord, start, end, infoList);
+                    }
+                }
+            }
+        }else {
+            //之前没做过其余查询，直接对所有数据操作
+            List<Map> mapList1 = new Query().doclist_sta(update, 0, 0);
+
+            for (Map map : mapList1) {
+                infoList = (List<Basic_info>) map.get("features");
+            }
+
+            if(startTime != null && endTime != null){
+                int start = Integer.parseInt(startTime);
+                int end = Integer.parseInt(endTime);
+                mapList = new Query().search_sec(searchWord, searchWord, start, end, infoList);
+            }else {
+                if(startTime == null && endTime == null){
+                    int start = 0;
+                    int end = 0;
+                    mapList = new Query().search_sec(searchWord, searchWord, start, end, infoList);
+                }else {
+                    if (startTime == null && endTime != null){
+                        int start = 0;
+                        int end = Integer.parseInt(endTime);
+                        mapList = new Query().search_sec(searchWord, searchWord, start, end, infoList);
+                    }else {
+                        int start = Integer.parseInt(startTime);
+                        int end = 0;
+                        mapList = new Query().search_sec(searchWord, searchWord, start, end, infoList);
+                    }
                 }
             }
         }
